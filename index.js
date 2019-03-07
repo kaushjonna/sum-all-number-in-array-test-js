15a0af23fb9b54282fdb7e41a27aba71684e0e64
Kaush <kaush.jonna@gmail.com>
2019-03-07 11:52:54 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum =0;
+
+  for(items of array){
+    if(Array.isArray(items)){
+      sum+= (sumItems(items));
+    } else {
+      sum += items;
+    }
+  }
+  return sum;
 }
+
+console.log(sumItems([1,2,[1,2,3]]));
 
 module.exports = sumItems;
